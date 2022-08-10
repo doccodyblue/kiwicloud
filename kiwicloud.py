ada1b2a9d9d0b1c18ede8772c1853d7438a7ec6f
doccodyblue <doccodyblue@gmail.com>
2022-08-10 20:02:08 +0200

    changed select for qrg cloud

--- kiwicloud.py
+++ kiwicloud.py
@@ -105,7 +105,7 @@ class db:
         return conhash
 
     def readQrgFrequency(self):
-        self.cursor.execute("SELECT frequency || ' ' || upper(mode), counter FROM qrgstat LIMIT 12")
+        self.cursor.execute("SELECT frequency || ' ' || upper(mode), counter FROM qrgstat ORDER BY counter DESC LIMIT 16")
         data = self.cursor.fetchall()
         return dict(data)
 
